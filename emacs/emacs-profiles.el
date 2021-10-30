231cb1b10de392080790ce079218d1096c825872
Thomas Stenhaug <thomas.stenhaug@gmail.com>
2021-10-30 13:59:42 +0200

    Adds emacs-profiles source

---
+++ emacs/emacs-profiles.el
@@ -0,0 +1,6 @@
+(("default"   . ((user-emacs-directory . "~/emacs/emacs.d/ke-doom")
+				 (env . (("DOOMDIR" . "~/emacs/doom.d/ke")))))
+ ("vanilla-doom" . ((user-emacs-directory . "~/emacs/emacs.d/vanilla-doom")
+				 (env . (("DOOMDIR" . "~/emacs/doom.d/vanilla")))))
+ ("vanilla" . ((user-emacs-directory . "~/emacs/emacs.d/vanilla"))))
+    
