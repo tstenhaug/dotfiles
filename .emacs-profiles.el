3569863223a21177bf076a0a042f1480c6273c07
Thomas Stenhaug <thomas.stenhaug@gmail.com>
2021-11-08 11:51:59 +0100

    Use ~/.emacs-profiles as a proper file

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,1 +1,6 @@
-emacs/emacs-profiles.el
+(("default"   . ((user-emacs-directory . "~/emacs/emacs.d/ke-doom")
+				 (env . (("DOOMDIR" . "~/emacs/doom.d/ke")))))
+ ("vanilla-doom" . ((user-emacs-directory . "~/emacs/emacs.d/vanilla-doom")
+				 (env . (("DOOMDIR" . "~/emacs/doom.d/vanilla")))))
+ ("vanilla" . ((user-emacs-directory . "~/emacs/emacs.d/vanilla"))))
+    
--- emacs/emacs-profiles.el
+++
@@ -1,6 +0,0 @@
-(("default"   . ((user-emacs-directory . "~/emacs/emacs.d/ke-doom")
-				 (env . (("DOOMDIR" . "~/emacs/doom.d/ke")))))
- ("vanilla-doom" . ((user-emacs-directory . "~/emacs/emacs.d/vanilla-doom")
-				 (env . (("DOOMDIR" . "~/emacs/doom.d/vanilla")))))
- ("vanilla" . ((user-emacs-directory . "~/emacs/emacs.d/vanilla"))))
-    
