d9db853c2f8cd7db39e4ea3bd912627812e1f47b
Thomas Stenhaug <thomas.stenhaug@gmail.com>
2021-11-12 22:49:07 +0100

    Enable docker and ansible modules

--- emacs/doom.d/ke/init.el
+++ emacs/doom.d/ke/init.el
@@ -87,10 +87,10 @@
        (flyspell +spell)
 
        :tools
-       ;;ansible
+       ansible
        debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
+       docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
