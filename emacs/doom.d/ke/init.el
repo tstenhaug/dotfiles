b4790ae6c904db276098b10a5264228ad1affe8f
Thomas Stenhaug <thomas.stenhaug@gmail.com>
2022-06-24 15:14:26 +0200

    Disable ligatures alltogether.
    More pain than gain.

--- emacs/doom.d/ke/init.el
+++ emacs/doom.d/ke/init.el
@@ -38,7 +38,7 @@
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        ;;indent-guides     ; highlighted indent columns
-       ligatures
+       ;; ligatures
        modeline    ; snazzy, Atom-inspired modeline, plus API
        nav-flash   ; blink the current line after jumping
        ;;neotree           ; a project drawer, like NERDTree for vim
